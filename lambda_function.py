dd6966eaad13a08f7cf7a8ff40fe7d03d51a38cf
Armando-Castillo <armandocreyes02@gmail.com>
2021-07-22 17:29:10 -0500

    read csv

--- lambda_function.py
+++ lambda_function.py
@@ -14,11 +14,21 @@ def lamba_handler(event, context):
     bucket_name = 'bucket_name'
     csv_list_items = [] #For read rows in the csv file
 
-    #Connections
-    s3 = boto3.client('s3')
+    #Connections -> https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+    s3 = boto3.client('s3') #S3 only can be a client
+    dynamo = boto3.client('dynamodb', region_name = region) #Could be resource or client (different ops)
 
-    ######CSV EXAMPLE######
-
+    ######CSV EXAMPLE READ DIRECTORIES/FULL BUCKET######
+    #Directories don´t exist on S3, but prefix are used
+    #Map keys (direction of objects in s3) from a bucket and prefix or directory dir1/dir2/
+    for key in s3.list_objects(Bucket = bucket_name, Prefix='dir1/dir2')['Contents']: #Returns directories
+        llave = key['Key'] #Other directory
+        #Read one csv file
+        csv_file = s3.get_object(Bucket = bucket_name, Key = llave)
+        #Split rows
+        csv_list_items = csv_file['Body'].read().decode('utf-8').split('\n')   
+        #Split values by separator or commas
+        csv_reader = csv.reader(csv_list_items, delimiter=',', quotechar='*')
 
     ######JSON EXAMPLE#####
 
